d8c1632a6651bd9d80e943640122b80ad1736476
saiganesh419 <37046497+saiganesh419@users.noreply.github.com>
2018-05-22 09:32:24 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
-#creating movie class
+# creating movie class
+
+
 class Movie():
-    def __init__(self,movie_title,movie_type,poster,youtube_url):
-        self.title=movie_title
-        self.storyline=movie_type
-        self.poster_image_url=poster
-        self.trailer_youtube_url=youtube_url
-#defining for to show youtube tariler
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        def __init__(self, movie_title, movie_type, poster, youtube_url):
+            self.title = movie_title
+            self.storyline = movie_type
+            self.poster_image_url = poster
+            self.trailer_youtube_url = youtube_url
+
+# defining for to show youtube tariler
+        def show_trailer(self):
+                webbrowser.open(self.trailer_youtube_url)
